ae9d1947f36a14bd98759b979e3ac57094988f8e
Aman <amanfarhan2008@gmail.com>
2025-08-06 23:33:29 +0500

    jnsdf

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ let second = rightNow.getSeconds();
 let mints = rightNow.getMinutes();
 let hours = rightNow.getHours();
 let miliSecond = rightNow.getMilliseconds();
-let time = rightNow.getTime();
+let time = rightNow.getTime()
 
 console.log(thisYear);
 console.log(hours);
