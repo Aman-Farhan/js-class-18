4df2df7e99a48700af32a645572968a8a195407b
Aman <amanfarhan2008@gmail.com>
2025-08-06 23:07:00 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+let rightNow = new Date();
+// let today = rightNow.getDay();
+
+// console.log(today,rightNow);
+
+
+
+// let month = rightNow.getMonth();
+// let months = ["jan","feb","mar","apr", "may","jun", "july","aug","sep","octuber"];
+// let monthStr = months[month];
+// console.log(monthStr);
+
+
+
+// let thisYear = rightNow.getFullYear();
+// console.log(thisYear);
+
+
+// let month = rightNow.getMonth()
+// let months = ["jan","feb","march","aprail","may","jun","july","aug","sep","octuber","november","december"];
+// let monthStr = months[month];
+// console.log(monthStr);
+
+
+// let date = rightNow.getDate();
+// console.log(date);
+
+
+let thisYear = rightNow.getFullYear();
+let second = rightNow.getSeconds();
+let mints = rightNow.getMinutes();
+let hours = rightNow.getHours();
+let miliSecond = rightNow.getMilliseconds();
+let time = rightNow.getTime();
+
+console.log(thisYear);
+console.log(hours);
+console.log(mints);
+console.log(second);
+console.log(miliSecond);
+console.log(time);
+console.log(rightNow);
